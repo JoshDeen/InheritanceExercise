a4754ecf47717e7978c3b45138324c18c7779837
Joshua Wilson <joshdeenwilson@gmail.com>
2024-06-12 16:55:23 -0400

    Exerecise Completed

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Bird : Animal
+    {
+
+       public bool HasFeathers { get; set; } = true;
+
+       public bool HasBeak { get; set; } = true;
+
+       public string Sound { get; set; }    
+
+       public bool EatsFish { get; set; }
+
+       public void PrintBirdDetails() 
+        {
+            Console.WriteLine($"Has feathers: {HasFeathers}");
+            Console.WriteLine($"Has a beak: {HasBeak}");
+            Console.WriteLine($"Voice sounds like: {Sound}");
+            Console.WriteLine($"Eats fish: {EatsFish}");
+
+        }
+
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,33 +6,55 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This info is for the bird:");
+            Bird animal1 = new Bird();
+
+            animal1.Diet = "Worms";
+            animal1.EatsFish = true;
+            animal1.Habitat = "Nest";
+            animal1.Sound = "Tweet";
+
+         animal1.PrintBirdDetails();
+            Console.WriteLine();
+            Console.WriteLine("This info is for the reptile:");
+
+            Reptile animal2 = new Reptile();
+
+            animal2.HasLegs = true;
+            animal2.IsCarnivor = true;
+            animal2.Water = "Swamp"; 
+            animal2.HasForkedTongue = true;
+
+         animal2.PrintReptileDetails();
+            
+            
             // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            // Create a class Animal - DONE
+            // give this class 4 members that all Animals have in common - DONE
 
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
+            // Create a class Bird - DONE
+            // give this class 4 members that are specific to Bird - DONE
+            // Set this class to inherit from your Animal Class - DONE
 
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
+            // Create a class Reptile - DONE
+            // give this class 4 members that are specific to Reptile - DONE
+            // Set this class to inherit from your Animal Class - DONE
 
 
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
+
+
+            /*Create an object of your Bird class - DONE
+             *  give values to your members using the object of your Bird class - DONE
              *  
-             * Creatively display the class member values 
+             * Creatively display the class member values - DONE
              */
 
             /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
+             *  give values to your members using the object of your Reptile class - DONE
              *  
-             * Creatively display the class member values 
+             * Creatively display the class member values - DONE
              */
         }
     }
